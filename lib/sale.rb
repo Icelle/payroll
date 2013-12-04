97d8e1b68299b2ad1fb6ef3b713ea992d547d945
Icelle <icelleku@gmail.com>
2013-12-03 23:01:12 -0500

    git hub swallowed all my code.

--- lib/sale.rb
+++ lib/sale.rb
@@ -1,18 +1,21 @@
 require 'csv'
 
 class Sale
-  def initialize
-    load_sales_data
+  attr_reader :gross_sale_value
+  def initialize(gross_sale_value)
+    @gross_sale_value = gross_sale_value
   end
 
-  def load_sales_data(filename = './assets/sales_data.csv')
-    sales_data = []
+  def self.load(filename = './assets/sales_data.csv')
+    sales_data = {}
     csv_table = CSV.table(filename).to_a
     csv_table.delete_at(0)
     csv_table.each do |row|
-      last_name          = row[0]
-      gross_sale_value   = row[1]
-  end
+      last_name             = row[0]
+      gross_sale_value      = row[1]
+      sales_data[last_name] ||= []
+      sales_data[last_name] << Sale.new(gross_sale_value)
+    end
     sales_data
-
+  end
 end
